04e0013b93abb187f67c58f6e083e1a72828fe53
Will Allan <will.allan@bamtechmedia.com>
2019-04-06 19:10:01 -0700

    Add license keys

--- src/demo2/assets/scripts/index.js
+++ src/demo2/assets/scripts/index.js
@@ -1,12 +1,12 @@
 new fullpage("main", {
-    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+    licenseKey: "9E3D4FA8-CC7D4361-84F62357-1D7AC074",
     menu: "nav",
     anchors: ["home", "fun", "family", "adventure"],
     sectionSelector: "section",
     slideSelector: "aside",
     slidesNavigation: true,
     parallax: true,
-    parallaxKey: "c2hlbGZseW5lLmNvbS5hdV8zMXJjR0Z5WVd4c1lYZz1BcHA=",
+    parallaxKey: "Y2FtcGdvZ2Fib2IuY29tX3RFU2NHRnlZV3hzWVhnPXg5ag==",
     parallaxOptions: { type: "reveal", percentage: 62, property: "translate" },
     scrollingSpeed: 1000,
     autoScrolling: true,
@@ -14,14 +14,20 @@ new fullpage("main", {
     fitToSection: false
 });
 
-document.getElementById("nav-toggle").addEventListener("click", function (e) {
-    this.classList.toggle("open");
-    document.querySelector("nav").classList.toggle("open");
-});
+document.getElementById("nav-toggle").addEventListener("click", () => toggleNav());
+document.querySelectorAll("nav>a").forEach((item) => item.addEventListener("click", () => closeNav()));
 
-document.querySelectorAll("nav>a").forEach(function (item, index) {
-    item.addEventListener("click", function (e) {
-        document.getElementById("nav-toggle").classList.remove("open");
-        document.querySelector("nav").classList.remove("open");
-    })
-});
+function openNav() {
+    document.getElementById("nav-toggle").classList.add("open");
+    document.querySelector("nav").classList.add("open");
+}
+
+function closeNav() {
+    document.getElementById("nav-toggle").classList.remove("open");
+    document.querySelector("nav").classList.remove("open");
+}
+
+function toggleNav() {
+    document.getElementById("nav-toggle").classList.toggle("open");
+    document.querySelector("nav").classList.toggle("open");
+}
